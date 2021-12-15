c1a31d246840ef1e0d60ee99885784d2547308fe
Tamal Saha <tamal@appscode.com>
2021-12-14 16:22:51 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ gql/README.md
@@ -0,0 +1,4 @@
+
+- https://medium.com/the-marcy-lab-school/how-to-use-dataloader-js-9727c527efd0
+- https://xuorig.medium.com/the-graphql-dataloader-pattern-visualized-3064a00f319f
+- https://gajus.medium.com/using-dataloader-to-batch-requests-c345f4b23433
--- gql/main.go
+++ gql/main.go
@@ -3,17 +3,125 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	apiv1 "kmodules.xyz/client-go/api/v1"
 	"log"
 	"net/http"
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/graphql/examples/todo/schema"
+	"github.com/graphql-go/graphql/testutil"
 	"github.com/graphql-go/handler"
 )
 
+// https://github.com/graphql-go/graphql/blob/master/examples/star-wars/main.go
+func main() {
+	var (
+		oidType *graphql.Object
+	)
+
+	oidType = graphql.NewObject(graphql.ObjectConfig{
+		Name:        "ObjectID",
+		Description: "Uniquely identifies a Kubernetes object",
+		Fields: graphql.Fields{
+			"group": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "The group of the Object",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if obj, ok := p.Source.(apiv1.ObjectID); ok {
+						return obj.Group, nil
+					}
+					return nil, nil
+				},
+			},
+			"kind": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "The kind of the Object",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if obj, ok := p.Source.(apiv1.ObjectID); ok {
+						return obj.Kind, nil
+					}
+					return nil, nil
+				},
+			},
+			"namespace": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "The namespace of the Object",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if obj, ok := p.Source.(apiv1.ObjectID); ok {
+						return obj.Namespace, nil
+					}
+					return nil, nil
+				},
+			},
+			"name": &graphql.Field{
+				Type:        graphql.String,
+				Description: "The name of the human.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if obj, ok := p.Source.(apiv1.ObjectID); ok {
+						return obj.Name, nil
+					}
+					return nil, nil
+				},
+			},
+			//"friends": &graphql.Field{
+			//	Type:        graphql.NewList(characterInterface),
+			//	Description: "The friends of the human, or an empty list if they have none.",
+			//	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			//		if human, ok := p.Source.(StarWarsChar); ok {
+			//			return human.Friends, nil
+			//		}
+			//		return []interface{}{}, nil
+			//	},
+			//},
+			//"appearsIn": &graphql.Field{
+			//	Type:        graphql.NewList(episodeEnum),
+			//	Description: "Which movies they appear in.",
+			//	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			//		if human, ok := p.Source.(StarWarsChar); ok {
+			//			return human.AppearsIn, nil
+			//		}
+			//		return nil, nil
+			//	},
+			//},
+			//"homePlanet": &graphql.Field{
+			//	Type:        graphql.String,
+			//	Description: "The home planet of the human, or null if unknown.",
+			//	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			//		if human, ok := p.Source.(StarWarsChar); ok {
+			//			return human.HomePlanet, nil
+			//		}
+			//		return nil, nil
+			//	},
+			//},
+		},
+	})
+	oidType.AddFieldConfig("links", &graphql.Field{
+		Type:        graphql.NewList(oidType),
+		Description: "Links from this object",
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			if obj, ok := p.Source.(apiv1.ObjectID); ok {
+				return obj.Friends, nil
+			}
+			return []interface{}{}, nil
+		},
+	})
+
+	h := handler.New(&handler.Config{
+		Schema:     &testutil.StarWarsSchema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+
+	http.Handle("/", h)
+	log.Println("server running on port :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+// go run gql/*.go
 // https://wehavefaces.net/learn-golang-graphql-relay-1-e59ea174a902
 // https://github.com/graphql-go/graphql/pull/574
-func main() {
+func main__() {
 	h := handler.New(&handler.Config{
 		Schema:     &schema.TodoSchema,
 		Pretty:     true,
@@ -21,7 +129,7 @@ func main() {
 		Playground: true,
 	})
 
-	http.Handle("/graphql", h)
+	http.Handle("/", h)
 	log.Println("server running on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
