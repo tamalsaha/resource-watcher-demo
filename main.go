699da95ef325dee97031c7352095ef4bf3831d91
Tamal Saha <tamal@appscode.com>
2021-12-15 22:12:49 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -33,7 +33,6 @@ import (
 	"gomodules.xyz/sets"
 	ksets "gomodules.xyz/sets/kubernetes"
 	policyv1 "k8s.io/api/policy/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -47,11 +46,9 @@ import (
 	"kmodules.xyz/authorizer/rbac"
 	apiv1 "kmodules.xyz/client-go/api/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var (
@@ -139,21 +136,6 @@ func main() {
 		// os.Exit(1)
 	}
 
-	r := reconcile.Func(func(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
-		return reconcile.Result{}, nil
-	})
-	if err := builder.ControllerManagedBy(mgr).For(&rbacv1.ClusterRole{}).Complete(r); err != nil {
-		panic(err)
-	}
-	if err := builder.ControllerManagedBy(mgr).For(&rbacv1.ClusterRoleBinding{}).Complete(r); err != nil {
-		panic(err)
-	}
-	if err := builder.ControllerManagedBy(mgr).For(&rbacv1.Role{}).Complete(r); err != nil {
-		panic(err)
-	}
-	if err := builder.ControllerManagedBy(mgr).For(&rbacv1.RoleBinding{}).Complete(r); err != nil {
-		panic(err)
-	}
 	rbacAuthorizer := rbac.NewForManagerOrDie(ctx, mgr)
 	fmt.Println(rbacAuthorizer)
 
